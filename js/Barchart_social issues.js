c065ddaf4ecf325023907065cc4d8d17e5053088
Yuan Fang <42786261+yuanfang313@users.noreply.github.com>
2019-12-11 19:38:37 -0500

    Update Barchart_social issues.js

--- js/Barchart_social issues.js
+++ js/Barchart_social issues.js
@@ -1,7 +1,7 @@
 /*jshint esversion: 6 */
 async function drawBarChart() {
   //1_Access data
-  const dataset = await d3.json("../th2_social issues.json")
+  const dataset = await d3.json("./worldoncover/th2_social issues.json")
   const xAccessor = d => d.proportion
   const nameAccessor = d => d.subCategory
   const imgAccessor = d =>d.trendImage
