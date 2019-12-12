1edd221cc122c76e1b6ce31af49c54383e9b7832
Yuan Fang <42786261+yuanfang313@users.noreply.github.com>
2019-12-11 19:42:21 -0500

    Update Barchart_figures.js

--- js/Barchart_figures.js
+++ js/Barchart_figures.js
@@ -1,7 +1,7 @@
 /*jshint esversion: 6 */
 async function drawBarChart() {
   //1_Access data
-  const dataset = await d3.json("../th2_figures.json")
+  const dataset = await d3.json("./worldoncover/th2_figures.json")
   const xAccessor = d => d.proportion
   const nameAccessor = d => d.subCategory
 
